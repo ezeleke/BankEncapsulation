042e62a9bf348ec7402b376bfdad6801e6b8d8b5
ezeleke <abefikru@gmail.com>
2022-08-26 09:55:07 -0700

    first commit

---
+++ BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+        private double balance = 0;
+        
+        public double Balance { get { return balance; } set { balance = value; } }
+
+        public void Deposit (double amount ) 
+        { 
+            balance = amount; 
+        }
+
+        public double GetBalance() 
+        {
+            return balance; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace BankEncapsulation
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            var account = new BankAccount();
+           
+            Console.WriteLine("How much would you like to deposit?");
+
+#pragma warning disable CS8604 // Possible null reference argument.
+            var amountToDeposit = double.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"Thank you!  your balance is now {account.GetBalance()}");
+        }
+    }
+}
